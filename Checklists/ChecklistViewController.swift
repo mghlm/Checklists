0d4b6c042fa93d5bd63618853afd389872d9fb99
Magnus Holm <mail@magnusholm.com>
2017-11-21 18:46:32 +0000

    Populate cell with hardcoded text

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,11 +21,20 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+        label.text = "Run a marathon"
+        } else {
+            label.text = "Sleep"
+        }
+        
         return cell 
     }
 
